861b358b9df64808a72a473482d7411c909f26e8
Xuan Thanh <thanh26092000@gmail.com>
2022-03-26 17:57:27 +0700

    model_home_work

--- src/main/java/com/virtualclassroom/model/Homework.java
+++ src/main/java/com/virtualclassroom/model/Homework.java
@@ -11,38 +11,34 @@ public class Homework {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long idClass;
-    private Long idHomeWork;
+    private Long Id;
+    private Long classId;
 
     @ManyToMany()
-    @JoinTable(name = "UserClass",
-            joinColumns = @JoinColumn(name = "workId", referencedColumnName = "id"),
-            inverseJoinColumns = @JoinColumn(name = "roleId", referencedColumnName = "id"))
+    @JoinTable(name = "HomeworkUser",
+            joinColumns = @JoinColumn(name = "homeWorkId", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "userId", referencedColumnName = "id"))
     private Set<Classroom> classrooms = new HashSet<>();
 
-    public Homework() {
-
+    public Homework() {}
+    public Homework(Long classId) {
+        this.classId = classId;
     }
 
-    public Homework(Long idClass, Long idHomeWork) {
-        this.idClass = idClass;
-        this.idHomeWork = idHomeWork;
+    public Long getId() {
+        return Id;
     }
 
-    public Long getIdClass() {
-        return idClass;
+    public void setId(Long id) {
+        Id = id;
     }
 
-    public void setIdClass(Long idClass) {
-        this.idClass = idClass;
+    public Long getClassId() {
+        return classId;
     }
 
-    public Long getIdHomeWork() {
-        return idHomeWork;
-    }
-
-    public void setIdHomeWork(Long idHomeWork) {
-        this.idHomeWork = idHomeWork;
+    public void setClassId(Long classId) {
+        this.classId = classId;
     }
 
     @Override
@@ -50,11 +46,11 @@ public class Homework {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Homework homework = (Homework) o;
-        return Objects.equals(idClass, homework.idClass) && Objects.equals(idHomeWork, homework.idHomeWork);
+        return Objects.equals(Id, homework.Id) && Objects.equals(classId, homework.classId) && Objects.equals(classrooms, homework.classrooms);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(idClass, idHomeWork);
+        return Objects.hash(Id, classId, classrooms);
     }
 }
